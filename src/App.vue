fe8120c797a7ab77d9388aa352c342f28d76b423
justwana-fly <nicolaibarbarasa@gmail.com>
2024-04-09 16:35:48 +0200

    Rimosse le cartelle non necessarie generate, aggiunto npm install bootstrap axios @fortawesome/fontawesome-free sass nella console e visuallizo a schermo il title 'Hello World'

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita "La mia prima app con Vite!" -->
+<template>
+ <h1>{{ title }}</h1>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+       title: 'Hello World'
+      }
+    },
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita "La mia prima app con Vite!"
+
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
